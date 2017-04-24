e994677ebf5ebe7cae381439533c8ad0b9d15705
ProgKing <dhowarth161@gmail.com>
2017-04-23 20:13:43 -0700

    Changes to decrypt and encrypt

--- RSA.java
+++ RSA.java
@@ -1,35 +1,5 @@
-import java.math.*;
-import java.util.*;
 
-public class RSA
-{
-	static public final int KEY_LENGTH_BITS = 256;
-	static public final int KEY_CERTANTY = 0x64; //TODO: Actually find a good certanty value
-	/**
-	 * Creates a public/private key pair from two prime numbers
-	 */
-	static public KeyPair generateKeys(BigInteger p, BigInteger q)
-	{
-		//NOTE: Big integer math is ugly, but it's needed for large keys
-		//Mod value (n=p*q)
-		BigInteger n = p.multiply(q);
-		//Totient value (phi=(p-1)(q-1))
-		BigInteger phi = p.subtract(BigInteger.ONE).multiply(p.subtract(BigInteger.ONE));
-		//Public key exponent (1 < e < phi)
-		BigInteger e;
-		//All primes are coprime to each other, so just get a prime thats less than phi
-		do
-		{
-			e = new BigInteger(phi.bitLength(), random);
-		} while(e.compareTo(BigInteger.ONE) <= 0 || e.compareTo(phi) >= 0 || !e.gcd(phi).equals(BigInteger.ONE));
-		// d * e mod phi = 1
-		// d = 1 * e^-1 mod phi
-		BigInteger d = e.modInverse(phi);
-		//TODO: assert(d * e mod phi == 1);
-		return new KeyPair(n, e, d);
-	};
-	/**
-	
+import java.io.UnsupportedEncodingException;
 import java.math.*;
 import java.util.*;
 
@@ -75,24 +45,25 @@ public class RSA
 	 * @param	e 		The public key exponent
 	 * @param 	n		The public key modulus
 	 */
-	static public String encrypt(String message, BigInteger e, BigInteger n)
+	static public String encrypt(String message, BigInteger e, BigInteger n) throws UnsupportedEncodingException
 	{
             String finals = "";
-		//BigInteger m = new BigInteger(message.getBytes());
-               byte[] b = new byte[10];
-                byte[] a = message.getBytes();
+            BigInteger m = new BigInteger(message.getBytes("US-ASCII"));
+              // byte[] b = new byte[10];
+              //  byte[] a = message.getBytes();
               // BigInteger temp = new BigInteger(a);
-               for(int i = 0; i < a.length; i++){
-                   for(int j = 0; j < 4; j++){
-                      if(j < a.length)
-                    b[j] = a[4*i+j];
-                      
-                    }
+             //  for(int i = 0; i < a.length; i++){
+              //     for(int j = 0; j < 4; j++){
+               //       if(4*i+j < a.length){
+               //     b[j] = a[4*i+j];
+                //      }
+               //     }
               
-                   BigInteger temp = new BigInteger(b);
-		 finals += temp.modPow(e, n).toString();
+               //    BigInteger temp = new BigInteger(b);
+		finals = m.modPow(e, n).toString();
+                 
             
-                }
+                //}
                 return finals;
 		//return new String("Encrypted message goes here");
 	};
@@ -101,10 +72,24 @@ public class RSA
 	 * @param 	d		The private key exponent
 	 * @param 	n 		The public key modulus
 	 */
-	static public String decrypt(String message, BigInteger d, BigInteger n)
+	static public String decrypt(String message, BigInteger d, BigInteger n) throws UnsupportedEncodingException
 	{
-		BigInteger m = new BigInteger(message.getBytes());
-		return m.modPow(d, n).toString();
+            
+            String finals = "";
+             
+		BigInteger m = new BigInteger(message.getBytes("US-ASCII"));
+           
+               
+               m = m.modPow(d, n);
+              byte[] byt =  m.toByteArray();
+              char[] cha = new char[byt.length];
+              for(int i =0; i < byt.length; i++){
+               
+              cha[i] = (char) byt[i];
+              finals+= cha[i];
+                 
+              }
+		return finals;
 	};
 	/**
 	 * Random Number Generator used for prime generation
--- RsaED.java
+++ RsaED.java
@@ -1,104 +1,3 @@
-iimport java.io.*;
-import java.math.BigInteger;
-
-
-public class RsaED {
-
-		public static void main(String[] args)
-		{
-			
-			char choice;
-			String line = new String();
-			
-			try
-			{
-				
-				printMenu(); //Display main menu
-		    	InputStreamReader isr = new InputStreamReader (System.in);
-		    	BufferedReader stdin = new BufferedReader (isr);
-				do
-				{
-					line = stdin.readLine().trim();
-		        	choice = line.charAt(0);
-		        	choice = Character.toUpperCase(choice);
-		        	
-		        	if(line.length() == 1)
-		        	{
-		        		switch(choice)
-		        		{
-                                            
-		        			case 'A': 
-		        					RSA encrypt = new RSA();	
-                                                                KeyPair key = encrypt.generateKeys();
-                                                                System.out.println(RSA.encrypt("hello world", key.getPrivate(), key.getPublic()));
-                                                                
-                                                                //Generate primes and encrypt a message
-		        					//Prime generation and encryption functionality to be added
-		        				
-		        				break;
-		        		
-		        		
-		        		
-		        			case 'B':
-                                                    
-                                                    System.out.println("Enter your public Key:");
-                                                    line = stdin.readLine();
-                                                    BigInteger publicKey = new BigInteger(line);
-                                                     System.out.println("Enter your private Key:");
-                                                      line = stdin.readLine();
-                                                    BigInteger privateKey = new BigInteger(line);
-                                                    
-                                                                RSA mEncrypt = new RSA();	
-                                                                KeyPair mKey = mEncrypt.generateKeys(publicKey,privateKey);System.out.println("hello");
-                                                                System.out.println(mEncrypt.encrypt("hello world", mKey.getPrivate(), mKey.getPublic()));
-                                                                System.out.println("hello");
-                                                                //Manually enter primes and encrypt a message
-		        					
-		        					//encryption functionality 
-		        				
-		        				break;
-		        		
-		        			
-		        			case 'C': 
-                                                               // RSA decrypt = new RSA();	
-                                                    
-                                                               // KeyPair s = decrypt.generateKeys();
-                                                               // System.out.println(RSA.encrypt("hello world", key.getPrivate(), key.getPublic()));//Decrypt a message
-		        			
-		        					//Decryption functionality be added
-		        				
-		        				break;
-		        				
-		        			case 'Q': //Stop the loop and quit
-		        			
-		        				break;
-		        			
-		        		}
-		        	}
-			    
-				}
-				while(choice != 'Q');
-			}//end of try
-			
-			catch(Exception e)
-			{
-				System.out.println("IO Exception");
-			}
-		  }
-		
-		public static void printMenu()
-		{
-			System.out.println("\n\n\n");
-			System.out.print("RSA Encrypt Decrypt\n"+
-							 "-----------------------\n"+
-							 "Please choose an option\n"+
-							 "-----------------------\n" +
-							 "A. Generate primes and create encrypt a message\n"+
-							 "B. Enter Primes manually and encrypt a message\n"+
-							 "C. Decrypt a message\n" +
-							 "Q. Quit\n\n");
-		}
-}
 import java.io.*;
 import java.math.BigInteger;
 
@@ -110,6 +9,8 @@ public class RsaED {
 			
 			char choice;
 			String line = new String();
+                        KeyPair keys = new KeyPair();
+                        String finals = new String();
 			
 			try
 			{
@@ -131,8 +32,11 @@ public class RsaED {
 		        			case 'A': 
 		        					RSA encrypt = new RSA();	
                                                                 KeyPair key = encrypt.generateKeys();
-                                                                System.out.println(RSA.encrypt("hello world", key.getPrivate(), key.getPublic()));
-                                                                
+                                                                finals = RSA.encrypt("hello world", key.getPrivate(), key.getPublic());
+                                                                System.out.println(finals);
+                                                                keys.setMod(key.getMod());
+                                                                keys.setPrivate(key.getPrivate());
+                                                                keys.setPublic(key.getPrivate());
                                                                 //Generate primes and encrypt a message
 		        					//Prime generation and encryption functionality to be added
 		        				
@@ -150,9 +54,9 @@ public class RsaED {
                                                     BigInteger privateKey = new BigInteger(line);
                                                     
                                                                 RSA mEncrypt = new RSA();	
-                                                                KeyPair mKey = mEncrypt.generateKeys(publicKey,privateKey);System.out.println("hello");
+                                                                KeyPair mKey = mEncrypt.generateKeys(publicKey,privateKey);
                                                                 System.out.println(mEncrypt.encrypt("hello world", mKey.getPrivate(), mKey.getPublic()));
-                                                                System.out.println("hello");
+                                                               
                                                                 //Manually enter primes and encrypt a message
 		        					
 		        					//encryption functionality 
@@ -161,10 +65,10 @@ public class RsaED {
 		        		
 		        			
 		        			case 'C': 
-                                                               // RSA decrypt = new RSA();	
+                                                               	
                                                     
-                                                               // KeyPair s = decrypt.generateKeys();
-                                                               // System.out.println(RSA.encrypt("hello world", key.getPrivate(), key.getPublic()));//Decrypt a message
+                                                               //KeyPair s = RSA.generateKeys();
+                                                                System.out.println(RSA.decrypt(finals, keys.getPrivate(), keys.getMod()));//Decrypt a message
 		        			
 		        					//Decryption functionality be added
 		        				
